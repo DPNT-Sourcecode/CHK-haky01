1da5343471da4419054c224879087b356b2999bf
sbx_user1051 <sbx_user1051@169.254.29.178>
2024-11-27 15:18:49 +0000

    Wed Nov 27 15:18:49 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,6 +6,7 @@ def checkout(skus):
         "B": 0,
         "C": 0,
         "D": 0,
+        "E": 0,
     }
 
     for c in skus:
@@ -18,11 +19,13 @@ def checkout(skus):
     B_total = get_price_with_offer(sku_count["B"], 2, 45, 30)
     C_total = 20 * sku_count["C"]
     D_total = 15 * sku_count["D"]
+    E_total = 40 * sku_count["E"]
 
-    return A_total + B_total + C_total + D_total
+    return A_total + B_total + C_total + D_total + E_total
 
 
 def get_price_with_offer(item_count, offer_count, offer_price, base_price):
     quot, rem = divmod(item_count, offer_count)
 
     return quot * offer_price + rem * base_price
+
