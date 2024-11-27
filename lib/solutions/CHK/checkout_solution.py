08bbbc7d85e525c1d6c810f16f540d22132e28a3
sbx_user1051 <sbx_user1051@169.254.29.178>
2024-11-27 15:42:50 +0000

    Wed Nov 27 15:42:50 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,13 +15,14 @@ def checkout(skus):
         else:
             return -1
 
-    if sku_count["A"] < 5:
-        A_total = get_price_with_offer(sku_count["A"], 3, 130, 50)
-    else:
-        A_total = get_price_with_offer(sku_count["A"], 5, 200, 50)
+    A_offer_total, rem = get_price_with_offer(sku_count["A"], 5, 200)
+    A_total = A_offer_total
+
+    A_offer_total, rem = get_price_with_offer(rem, 3, 130)
+    A_total += A_offer_total + rem * 50
 
     B_to_pay = get_items_to_pay_for(sku_count["B"], sku_count["E"], 2)
-    B_total = get_price_with_offer(B_to_pay, 2, 45, 30)
+    B_offer_total, B_rem = get_price_with_offer(B_to_pay, 2, 45, 30)
 
     C_total = 20 * sku_count["C"]
     D_total = 15 * sku_count["D"]
@@ -30,10 +31,10 @@ def checkout(skus):
     return A_total + B_total + C_total + D_total + E_total
 
 
-def get_price_with_offer(item_count, offer_count, offer_price, base_price):
+def get_price_with_offer(item_count, offer_count, offer_price):
     quot, rem = divmod(item_count, offer_count)
 
-    return quot * offer_price + rem * base_price
+    return quot * offer_price, rem
 
 
 def get_items_to_pay_for(item_count, offer_count, offer_count_required, free_items=1):
@@ -41,4 +42,5 @@ def get_items_to_pay_for(item_count, offer_count, offer_count_required, free_ite
     item_count_to_pay = item_count - (offer_count // offer_count_required) * free_items
 
     return item_count_to_pay if item_count_to_pay >= 0 else 0
+
 
