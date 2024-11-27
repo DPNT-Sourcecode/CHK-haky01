87ad1e354d370a93dbdaec09a199eac5f663e47a
sbx_user1051 <sbx_user1051@169.254.5.130>
2024-11-27 17:48:06 +0000

    Wed Nov 27 17:48:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -247,21 +247,3 @@ def calc_multi_buy_free_other(sku):
 
     sku["count"] = new_count if new_count >= 0 else 0
 
-
-def get_price_with_offer(item_count, offer_count, offer_price):
-    quot, rem = divmod(item_count, offer_count)
-
-    return quot * offer_price, rem
-
-
-def get_items_to_pay_for(item_count, offer_count, offer_count_required, free_items=1):
-
-    item_count_to_pay = item_count - (offer_count // offer_count_required) * free_items
-
-    return item_count_to_pay if item_count_to_pay >= 0 else 0
-
-
-def get_same_item_free(item_count, offer_count_required, free_items=1):
-    item_count_to_pay = item_count - item_count // (offer_count_required + free_items)
-
-    return item_count_to_pay
