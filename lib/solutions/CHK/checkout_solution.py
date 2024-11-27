e4a2123f1a6a5703f0e11987f1c6dd179904f4f6
sbx_user1051 <sbx_user1051@169.254.29.178>
2024-11-27 17:01:19 +0000

    Wed Nov 27 17:01:19 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,20 +1,23 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    # Note no validation of discount orders yet
-    rules = {
-        "A": {
-            "price": 50,
-            "multi_buy_discount": [(5, 200), (3, 130)],
-            "multi_buy_free": (2, 1),
-            "multi_buy_free_other": (2, 1, "E"),
-            "count": 0,
-        },
-        "B": {
 
-        }
+# Note no validation of discount orders yet
+rules = {
+    "A": {
+        "price": 50,
+        "multi_buy_discount": [(5, 200), (3, 130)],
+        "multi_buy_free": (2, 1),
+        "multi_buy_free_other": (2, 1, "E"),
+        "count": 0,
+    },
+    "B": {
 
     }
+
+}
+
+def checkout(skus):
+
 
     for c in skus:
         if c in rules:
@@ -40,13 +43,16 @@ def get_multi_buy_discount_total(sku):
 
 def get_multi_buy_free(sku):
     count = sku["count"]
-    offer_count = sku["multi_buy_free"][0]
+    min_count = sku["multi_buy_free"][0]
     free_count = sku["multi_buy_free"][1]
 
+    return count - count // (min_count + free_count)
 
-
-    return count - count // (offer_count + free_count)
-
+def get_multi_buy_free_other(sku):
+    count = sku["count"]
+    item_count = rules[sku["multi_buy_free_other"][2]]["count"]
+    min_count =
+    free_count =
 
 
 
@@ -67,6 +73,7 @@ def get_same_item_free(item_count, offer_count_required, free_items=1):
     item_count_to_pay = item_count - item_count // (offer_count_required + free_items)
 
     return item_count_to_pay
+
 
 
 
