32d143e56887195928747ac3b62965ffce638f8d
sbx_user1051 <sbx_user1051@169.254.29.178>
2024-11-27 15:45:59 +0000

    Wed Nov 27 15:45:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,6 +7,7 @@ def checkout(skus):
         "C": 0,
         "D": 0,
         "E": 0,
+        "F": 0,
     }
 
     for c in skus:
@@ -17,7 +18,6 @@ def checkout(skus):
 
     A_offer_total, rem = get_price_with_offer(sku_count["A"], 5, 200)
     A_total = A_offer_total
-
     A_offer_total, rem = get_price_with_offer(rem, 3, 130)
     A_total += A_offer_total + rem * 50
 
@@ -29,7 +29,10 @@ def checkout(skus):
     D_total = 15 * sku_count["D"]
     E_total = 40 * sku_count["E"]
 
-    return A_total + B_total + C_total + D_total + E_total
+    F_to_pay = get_items_to_pay_for(sku_count["F"], sku_count["F"], 2)
+    F_total = F_to_pay * 10
+
+    return A_total + B_total + C_total + D_total + E_total + F_total
 
 
 def get_price_with_offer(item_count, offer_count, offer_price):
@@ -43,6 +46,7 @@ def get_items_to_pay_for(item_count, offer_count, offer_count_required, free_ite
     item_count_to_pay = item_count - (offer_count // offer_count_required) * free_items
 
     return item_count_to_pay if item_count_to_pay >= 0 else 0
+
 
 
 
