b63aaa3ccef4ff152f555fc5b213a8b402d01611
sbx_user1051 <sbx_user1051@169.254.29.178>
2024-11-27 15:12:39 +0000

    Wed Nov 27 15:12:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,6 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # Would be quite useful to know how the string is formatted
-
     sku_count = {
         "A": 0,
         "B": 0,
@@ -16,15 +14,19 @@ def checkout(skus):
         else:
             return -1
 
-    quot_A, rem_A = divmod(sku_count["A"], 3)
-    quot_B, rem_B = divmod(sku_count["B"], 2)
-
-    A_total = quot_A * 130 + rem_A * 50
-    B_total = quot_B * 45 + rem_B * 30
+    A_total = get_price_with_offer(sku_count["A"], 3, 130, 50)
+    B_total = get_price_with_offer(sku_count["B"], 2, 45, 30)
     C_total = 20 * sku_count["C"]
     D_total = 15 * sku_count["D"]
 
     return A_total + B_total + C_total + D_total
+
+
+def get_price_with_offer(item_count, offer_count, offer_price, base_price):
+    quot, rem = divmod(item_count, offer_count)
+
+    return quot * offer_price + rem * base_price
+
 
 
 
