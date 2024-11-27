ea2084406e3a40aaf4c48dc281e999a06cf80d18
sbx_user1051 <sbx_user1051@169.254.29.178>
2024-11-27 14:58:39 +0000

    Wed Nov 27 14:58:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,26 @@
-
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    # Would be quite useful to know how the string is formatted
+
+    sku_count = {
+        "A": 0,
+        "B": 0,
+        "C": 0,
+        "D": 0,
+    }
+
+    for c in skus:
+        if c == "A":
+            pass
+        elif c == "B":
+            pass
+        elif c == "C":
+            pass
+        elif c == "D":
+            pass
+        else:
+            pass
+
     raise NotImplementedError()
+
