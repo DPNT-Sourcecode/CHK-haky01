1effb1f3d38616441039bda74496d184f7d31c7a
sbx_user1051 <sbx_user1051@169.254.29.178>
2024-11-27 15:30:49 +0000

    Wed Nov 27 15:30:49 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,7 +20,7 @@ def checkout(skus):
     else:
         A_total = get_price_with_offer(sku_count["A"], 5, 200, 50)
 
-
+    B_to_pay = get_items_to_pay_for(sku_count["B"], sku_count["E"], 2)
 
     B_total = get_price_with_offer(sku_count["B"], 2, 45, 30)
 
@@ -36,7 +36,12 @@ def get_price_with_offer(item_count, offer_count, offer_price, base_price):
 
     return quot * offer_price + rem * base_price
 
-def get_free_item(item_count, offer_count, )
+
+def get_items_to_pay_for(item_count, offer_count, offer_count_required, free_items=1):
+    item_count_to_pay = item_count - offer_items / offer_count * free_items
+
+    return item_count_to_pay if item_count_to_pay >= 0 else 0
+
 
 
 
